e9bc846dd3bb8613d2c5961bd2d1210b854d6804
nikos papadakis <npapadakis@gmail.com>
2023-01-13 18:49:00 +0200

    small changes in script

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,25 +1,22 @@
 const { ethers, network } = require("hardhat")
 const { moveBlocks } = require("../utils/move-blocks")
 
-const PRICE = ethers.utils.parseEther("0.1")
-
-async function mintAndList() {
-    const basicNft = await ethers.getContract("BasicNftTwo")
-    console.log("Minting NFT...")
+async function mint() {
+    const basicNft = await ethers.getContract("BasicNft")
+    console.log("Minting...")
     const mintTx = await basicNft.mintNft()
     const mintTxReceipt = await mintTx.wait(1)
-    console.log(
-        `Minted tokenId ${mintTxReceipt.events[0].args.tokenId.toString()} from contract: ${
-            basicNft.address
-        }`
-    )
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+    console.log(`Got TokenID: ${tokenId}`)
+    console.log(`NFT Address: ${basicNft.address}`)
+
     if (network.config.chainId == 31337) {
         // Moralis has a hard time if you move more than 1 block!
         await moveBlocks(2, (sleepAmount = 1000))
     }
 }
 
-mintAndList()
+mint()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error)
